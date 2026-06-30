08740e158ad3ceef71cc938bc372115e7479a397
Ovopyriform <10652008+Ovopyriform@users.noreply.github.com>
2026-06-30 18:00:15 +0100

    Consolidate messages

--- openautomaker-i18n/src/main/java/org/openautomaker/environment/I18N.java
+++ openautomaker-i18n/src/main/java/org/openautomaker/environment/I18N.java
@@ -1,16 +1,10 @@
 package org.openautomaker.environment;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.Locale;
 import java.util.MissingResourceException;
-import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -22,46 +16,9 @@ import jakarta.inject.Singleton;
 @Singleton
 public final class I18N {
 
-	/**
-	 * ResourceBundle implementation used to consolidate multiple resource bundles
-	 */
-	private class MultiResourceBundle extends ResourceBundle {
-
-		private final List<ResourceBundle> delegates;
-
-		/**
-		 * Build a resource bundle from multiple resource bundles
-		 * 
-		 * @param resourceBundles - ResourceBundles to consolidate
-		 */
-		public MultiResourceBundle(ResourceBundle... resourceBundles) {
-			delegates = resourceBundles == null ? new ArrayList<>() : List.of(resourceBundles);
-		}
-
-		@Override
-		protected Object handleGetObject(String key) {
-			Optional<Object> firstPropertyValue = this.delegates.stream()
-					.filter(delegate -> delegate != null && delegate.containsKey(key))
-					.map(delegate -> delegate.getObject(key))
-					.findFirst();
-
-			return firstPropertyValue.isPresent() ? firstPropertyValue.get() : null;
-		}
-
-		@Override
-		public Enumeration<String> getKeys() {
-			List<String> keys = this.delegates.stream()
-					.filter(delegate -> delegate != null)
-					.flatMap(delegate -> Collections.list(delegate.getKeys()).stream())
-					.collect(Collectors.toList());
-
-			return Collections.enumeration(keys);
-		}
-	}
-
 	private static final Pattern TEMPLATE_PATTERN = Pattern.compile(".*\\*T(\\d\\d).*");
 
-	protected ResourceBundle fldLanguage = null;
+	protected ResourceBundle messages = null;
 
 	/**
 	 * Injectable I18N constructor
@@ -94,7 +51,7 @@ public final class I18N {
 
 		// Manage non-existing keys, just return the string passed to us.
 		try {
-			translatedResource = fldLanguage.getString(key);
+			translatedResource = messages.getString(key);
 		}
 		catch (MissingResourceException ex) {
 			return key;
@@ -131,7 +88,7 @@ public final class I18N {
 	 * @return The resources currently in use.
 	 */
 	public ResourceBundle getResourceBundle() {
-		return fldLanguage;
+		return messages;
 	}
 
 	/**
@@ -143,18 +100,8 @@ public final class I18N {
 		refreshResourceBundle(locale);
 	}
 
-	// TODO: Optimisation.  Resource Bundles are cached.  Combining the resources would mean there's no need for this multi-resource bundle
-	private void refreshResourceBundle(Locale locale) {
-		String packageName = getClass().getPackageName();
-		List<String> resourceNames = List.of(
-				packageName + ".LanguageData",
-				packageName + ".UI_LanguageData",
-				packageName + ".NoUI_LanguageData");
-
-		fldLanguage = new MultiResourceBundle(resourceNames.stream()
-				.map((resourceName) -> {
-					return ResourceBundle.getBundle(resourceName, locale);
-				})
-				.toArray(ResourceBundle[]::new));
+	private void refreshResourceBundle(Locale locale)
+	{
+		messages = ResourceBundle.getBundle(getClass().getPackageName() + ".messages", locale);
 	}
 }
